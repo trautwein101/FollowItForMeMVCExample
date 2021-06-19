a138a0b1027d8a53f8afc8944fad41abcfe98f3a
trautwein101 <trautwein101@yahoo.com>
2021-06-18 19:47:03 -0600

    Initial Setup
    With Single Authentication

---
+++ FollowItForMeMVCExample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FollowItForMeMVCExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
