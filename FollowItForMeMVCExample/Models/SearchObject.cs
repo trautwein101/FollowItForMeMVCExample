5633d4f84b185d74b03910466ff235d6019e8333
trautwein101 <trautwein101@yahoo.com>
2021-06-30 17:00:36 -0600

    Data Annotations Added

--- FollowItForMeMVCExample/Models/SearchObject.cs
+++ FollowItForMeMVCExample/Models/SearchObject.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace FollowItForMeMVCExample.Models
     public class SearchObject
     {
 
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string UserId { get; set; }
         public string Type { get; set; }
         public string Name { get; set; }
